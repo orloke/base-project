224b3574254940c1da74717a5da28c83a0a475e5
Junior Dering <juniord@lepvtech.com.br>
2025-07-26 13:54:15 -0300

    feat: test

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 
 export const Description = () => {
   const [number, setNumber] = useState(0);
+  console.log("number", number, process.env.NEXT_PUBLIC_FRONT_URL);
   return (
     <div className="text-white">
       <span>{process.env.NEXT_PUBLIC_FRONT_URL}</span>
